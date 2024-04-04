ed5b75cff81645d8276ece713869d13787aeaaf1
peterdz099 <piotrdziula@gmail.com>
2024-04-04 22:50:22 +0200

    set up django project

---
+++ oe2/oe2/generic_algorithm/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .forms import MyForm
+
+def my_view(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            pass  # Placeholder for form data processing
+    else:
+        form = MyForm()
+    return render(request, 'template_name.html', {'form': form})
