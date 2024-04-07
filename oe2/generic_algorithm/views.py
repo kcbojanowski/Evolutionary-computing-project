a1c57df8a8e9c2f1b2897295c5ed1d330e6c846f
unknown <piotrdziula@gmail.com>
2024-04-07 22:10:54 +0200

    add working endpoint

--- oe2/generic_algorithm/views.py
+++ oe2/generic_algorithm/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from .forms import MyForm
 
-def my_view(request):
-    if request.method == 'POST':
-        form = MyForm(request.POST)
-        if form.is_valid():
-            # Process the form data
-            pass  # Placeholder for form data processing
-    else:
-        form = MyForm()
-    return render(request, 'generic.html', {'form': form})
+def index(request):
+    if request.method == 'GET':
+        return render(request, 'generic.html')
