483016d809abbfed341ecf9326112de3c583035f
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2024-04-09 19:27:38 +0200

    Add DiscreteCrossover implementation

--- oe2/genetic_algorithm/crossovers/discrete_crossover.py
+++ oe2/genetic_algorithm/crossovers/discrete_crossover.py
@@ -1,9 +1,15 @@
 import random
 
-from genetic_algorithm.chromosomes.chromosome import Chromosome
-from genetic_algorithm.crossovers.crossover import Crossover
+from oe2.genetic_algorithm.chromosomes.chromosome import Chromosome
+from oe2.genetic_algorithm.crossovers.crossover import Crossover
 
 
 class DiscreteCrossover(Crossover):
     def crossover(self, chromosome1: Chromosome, chromosome2: Chromosome) -> Chromosome:
-        pass
+        child_binary_representation = []
+        for i in range(len(chromosome1.binary_representation)):
+            if random.random() <= 0.5:
+                child_binary_representation.append(chromosome1.binary_representation[i])
+            else:
+                child_binary_representation.append(chromosome2.binary_representation[i])
+        return Chromosome(child_binary_representation, chromosome1.left_boundary, chromosome1.right_boundary)
