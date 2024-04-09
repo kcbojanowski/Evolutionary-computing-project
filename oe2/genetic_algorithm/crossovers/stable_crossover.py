370af2b3abc41813c5ed922fa95d64f8c44e974e
unknown <piotrdziula@gmail.com>
2024-04-09 17:34:26 +0200

    add stable crossover

---
+++ oe2/genetic_algorithm/crossovers/stable_crossover.py
@@ -0,0 +1,22 @@
+import random
+
+from genetic_algorithm.chromosomes.chromosome import Chromosome
+from genetic_algorithm.crossovers.crossover import Crossover
+
+
+class StableCrossover(Crossover):
+    def crossover(self, chromosome1: Chromosome, chromosome2: Chromosome) -> Chromosome:
+        child_binary_representation = []
+        parent_a = chromosome1.binary_representation
+        parent_b = chromosome2.binary_representation
+        for i in range(len(chromosome1.binary_representation)):
+            if parent_a[i] == parent_b[i]:
+                child_binary_representation.append(parent_b[i])
+            else:
+                u = random.random()
+                if i <= 0.5:
+                    child_binary_representation.append(0)
+                else:
+                    child_binary_representation.append(1)
+
+        return Chromosome(child_binary_representation, chromosome1.left_boundary, chromosome1.right_boundary)
