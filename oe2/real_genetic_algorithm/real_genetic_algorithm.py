a69703b4d31859c1ca415a0689fe819cac3517f6
kcbojanowski <k1bojanowski@gmail.com>
2024-04-28 13:58:25 +0200

    real_algo, selections

--- oe2/real_genetic_algorithm/real_genetic_algorithm.py
+++ oe2/real_genetic_algorithm/real_genetic_algorithm.py
@@ -5,6 +5,7 @@ from typing import List, Tuple
 from timeit import default_timer as timer
 
 from real_genetic_algorithm.real_genetic_algorithm_configuration import RealGeneticAlgorithmConfiguration
+from real_genetic_algorithm.chromosomes.real_chromosome import generate_real_chromosomes
 
 
 class RealGeneticAlgorithm:
@@ -12,5 +13,25 @@ class RealGeneticAlgorithm:
         self.configuration = configuration
 
     def perform(self):
-        #TODO
-        pass
+        # Initialize population
+        population = generate_real_chromosomes(self.configuration.population_size, self.configuration.chromosome_dimension, self.configuration.left_boundary, self.configuration.right_boundary, self.configuration.fitness_function)
+
+        for _ in range(self.configuration.generations):
+            # Select parents
+            parents = self.configuration.selection_method.select(population)
+
+            # Generate offspring through crossover
+            offspring = [self.configuration.crossover_method.crossover(p1, p2) for p1, p2 in zip(parents[::2], parents[1::2])]
+
+            # Perform mutation
+            offspring = [self.configuration.mutation_method.mutation(child, self.configuration.mutation_probability) for child in offspring]
+
+            # Evaluate fitness
+            for individual in offspring:
+                individual.calculate_value()
+
+            # Select for the next generation
+            population = self.configuration.selection_method.select(population + offspring)
+
+        # Return the best solution
+        return max(population, key=lambda individual: individual.calculate_value())
--- oe2/real_genetic_algorithm/selections/real_selection.py
+++ oe2/real_genetic_algorithm/selections/real_selection.py
@@ -1,24 +1,39 @@
 from typing import List
-
-
+import numpy as np
 from real_genetic_algorithm.chromosomes.real_candidates import RealCandidates
 
 
 class RealSelection:
-    def select(self, population: RealCandidates, num_select: int, fitness_function: str):
+    def select(self, population: RealCandidates, num_select: int, fitness_function: str, maximization: bool):
         pass
 
 
 class RealBestSelection(RealSelection):
-    pass
+    def select(self, population: RealCandidates, num_select: int, fitness_function: str, maximization: bool):
+        sorted_population = sorted(population, key=lambda individual: individual.calculate_value(),
+                                   reverse=maximization)
+        return sorted_population[:num_select]
 
-    
 
 class RealRouletteWheelSelection(RealSelection):
-    pass
-   
+    def select(self, population: RealCandidates, num_select: int, fitness_function: str, maximization: bool):
+        fitness_values = [individual.calculate_value() for individual in population]
+        if not maximization:
+            fitness_values = [1.0 / max(f, 0.00001) for f in fitness_values]
+        total_fitness = sum(fitness_values)
+        probabilities = [fitness / total_fitness for fitness in fitness_values]
+        selected = np.random.choice(population, size=num_select, p=probabilities)
+        return selected
+
 
 class RealTournamentSelection(RealSelection):
-    pass
-
-    
+    def select(self, population: RealCandidates, num_select: int, fitness_function: str, maximization: bool):
+        selected = []
+        for _ in range(num_select):
+            participants = np.random.choice(population, size=num_select)
+            if maximization:
+                winner = max(participants, key=lambda individual: individual.calculate_value())
+            else:
+                winner = min(participants, key=lambda individual: individual.calculate_value())
+            selected.append(winner)
+        return selected
